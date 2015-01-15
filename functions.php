25bfa20ae62ce0d504c7d7d5c5ad9e34b5bd2f52
97world <97world@gmail.com>
2015-01-16 00:40:02 +0800

    --Improve:Change gravatar use ssl url. --Improve:The archive page display images then set image size invalid.

--- functions.php
+++ functions.php
@@ -790,4 +790,10 @@ return $commentdata;
 if($comment_data['comment_type']==''){
 add_filter('preprocess_comment','spam_protection_pre');
 }
+
+function get_ssl_avatar($avatar) {
+   $avatar = preg_replace('/.*\/avatar\/(.*)\?s=([\d]+)&.*/','<img src="https://secure.gravatar.com/avatar/$1?s=$2" class="avatar avatar-$2" height="$2" width="$2">',$avatar);
+   return $avatar;
+}
+add_filter('get_avatar', 'get_ssl_avatar');
 ?>
