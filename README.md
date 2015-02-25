625b479866ae8da4eb4a2b1743696ba4f252ebd9
97world <97world@gmail.com>
2015-02-25 21:38:36 +0800

    --Update:Update README.md file. --Update:Deleted useless code for footer.php file.

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 #ABOUT 97world_GWhite #
 
-这是一款Wordpress主题，由G-White修改而成。
+这是一款Wordpress主题，基于Alan Ouyang的G-White 3.0修改而成.
+很喜欢G-White的风格，但是我需要的一些功能这款主题并不具备，于是酷爱折腾的我对主题进行了修改.
+
+感谢Alan Ouyang制作了G-White这款主题！
+
+---
+
+## New Featrues（新增功能） ##
+
+* 首页文章列表支持自定义缩略图，如果没有设置会随机显示
+* 首页文章列表会根据是否有安装WP-Postview插件决定是否显示文章的浏览次数
+* 文章页面的细节调整，全局字体设置为微软雅黑
+* 增加Guestbook留言板页面模板，模板会根据评论次数生成访客的头像列表
+* 增加Archives日志归档页面模板，根据月份生成每个月份的文章列表
+* 增加Link链接页面模板，显示每个分类的链接(需要安装插件以及修改代码才能正常使用)
+* 显示Link链接的Widget，修改为两列显示
+* 修改Tag标签的Widget以支持随机彩色标签
+* 评论支持“评论回复邮件”功能，访客也可以选择不接收回复邮件
+* 评论增加算术验证码功能，访客再次评论不需要重复输入验证码
+* 调整Gravatar头像链接以便能在国内访问
+* 调整页面引用的jQuery库链接以便能在国内访问
+
+---
+
+## Contact Me （联系我） ##
+
+BLOG：  http://www.97world.com/
+
+Twitter：https://twitter.com/97world
+
+Weibo： http://weibo.com/80378091
+
+Email:  97world#Gmail.Com（将#替换成@）
+
+---
+
+## Support My Work （支持我的工作） ##
+
+PayPal:97world#Gmail.Com （将#替换成@）
+
+AliPay:https://me.alipay.com/97world
--- footer.php
+++ footer.php
@@ -24,9 +24,6 @@
 
 			<div id="site-info">
 				<span class="wp_credit"><a href="<?php echo esc_url( __('http://wordpress.org/', 'twentyten') ); ?>" title="<?php esc_attr_e('Semantic Personal Publishing Platform', 'twentyten'); ?>" rel="generator"><?php _e('Powered By Wordpress', 'Twenty Ten' ); ?></a></span> Copyright &copy; 2010 <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>. Theme G-White by Alan Ouyang.
-				<script src="http://s9.cnzz.com/stat.php?id=3827267&web_id=3827267" language="JavaScript"></script>.
-				<script type="text/javascript">var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fcfe25940b12793e62f3a5b168285e0bc' type='text/javascript'%3E%3C/script%3E"));</script>.
-				<a href="http://www.97world.com/sitemap.html" target="_blank">&#x7F51;&#x7AD9;&#x5730;&#x56FE;</a>.
 			</div><!-- #site-info -->
 
 			<div id="site-generator">
